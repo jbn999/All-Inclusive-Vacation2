bb745d52ca3b7234df45012fa2328545ac87391a
Jared Norton <jared.b.norton@outlook.com>
2023-03-27 23:44:09 -0400

    Add Hero section for index and About page.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'All Inclusive Vacation Info';
+export const SITE_DESCRIPTION = 'Discover the perfect all-inclusive vacation with AllInclusiveVacationInfo.com! Explore top resorts, destination guides, expert travel tips, and honest reviews tailored for couples, families, and solo travelers.';
